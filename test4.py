f404a842d3dcdc8278860410f2c4005c13e06037
PY2-22daniel <98322989+PY2-22daniel@users.noreply.github.com>
2022-01-25 10:13:58 +0800

    第一天
    打招呼 input

---
+++ test4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 11:45:15 2022
+
+@author: 10419
+"""
+
+name = input('what is your name?')
+
+#print(name)
+
+print('hello' ,name)
---
+++ test5.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 21:05:46 2022
+
+@author: 10419
+"""
+
+import math
+
+print(math.pow(99999,9))
